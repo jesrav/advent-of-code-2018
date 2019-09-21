571df418ae853423132fc0d49b135b54fbd00267
Ravnbøl Jes <jerav@DFDS.COM>
2019-09-21 23:24:39 +0200

    wip

---
+++ day3.py
@@ -0,0 +1,11 @@
+from utils import load_data
+
+claims_input = load_data('input_day3.txt')
+
+test_data = [
+    '#1 @ 1,3: 4x4',
+    '#2 @ 3,1: 4x4',
+    '#3 @ 5,5: 2x2',
+]
+
+print(claims_input)
